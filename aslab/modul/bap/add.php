106fda6c066804bc088537588bc957205bb6429e
raihan <raihanpresiden2003@gmail.com>
2023-09-27 16:39:55 +0800

    perbarui fitur buat BAP

--- aslab/modul/bap/add.php
+++ aslab/modul/bap/add.php
@@ -32,17 +32,45 @@
                     <form action="?page=aslab&act=proses" method="post" enctype="multipart/form-data">
                         <div class="form-group">
                             <label>Jenjang</label>
-                            <input name="jenjang" type="text" class="form-control" placeholder="S1">
+                            <select name="jenjang" class="form-control">
+                                <option value="">- Pilih -</option>
+                                <?php
+                                $jenjang = mysqli_query($con, "SELECT * FROM tb_jenjang ORDER BY id_jenjang ASC");
+                                foreach ($jenjang as $j) {
+                                    echo "<option value='$j[id_jenjang]'> $j[nama_jenjang]</option>";
+                                    ?>
+                                <?php } ?>
+                            </select>
                         </div>
 
                         <div class="form-group">
-                            <label>Jurusan</label>
-                            <input name="jurusan" type="text" class="form-control" placeholder="Teknik Informatika">
+                            <label for="">Jurusan</label>
+
+                            <select name="program_studi" class="form-control">
+                                <option value="">- Pilih -</option>
+                                <?php
+                                $program_studi = mysqli_query($con, "SELECT * FROM tb_program_studi ORDER BY id_program_studi ASC");
+                                foreach ($program_studi as $ps) {
+                                    echo "<option value='$ps[id_program_studi]'> $ps[nama_program_studi]</option>";
+                                }
+                                ?>
+                            </select>
+
                         </div>
 
+                        <!-- Matakuliah -->
                         <div class="form-group">
-                            <label>Nama Mata Kuliah Praktikum</label>
-                            <input name="matkul" type="text" class="form-control" placeholder="Pemrograman Web">
+                            <label>Matakuliah</label>
+                            <select name="matakuliah" class="form-control" required>
+                                <option value="">- Pilih -</option>
+                                <?php
+                                $matakuliah = mysqli_query($con, "SELECT * FROM tb_matakuliah ORDER BY id_matakuliah ASC");
+                                foreach ($matakuliah as $g) {
+                                    echo "<option value='$g[id_matakuliah]'>[ $g[kode_matakuliah] ] $g[matakuliah]</option>";
+                                }
+                                ?>
+
+                            </select>
                         </div>
 
                         <div class="form-group">
@@ -50,14 +78,32 @@
                             <input name="sks" type="text" class="form-control" placeholder="2">
                         </div>
 
+                        <!-- Kelas -->
                         <div class="form-group">
                             <label>Kelas</label>
-                            <input name="kelas" type="text" class="form-control" placeholder="Pagi A">
+                            <select name="kelas" class="form-control">
+                                <option value="">- Pilih -</option>
+                                <?php
+                                $kelas = mysqli_query($con, "SELECT * FROM tb_kelompok ORDER BY id_kelompok ASC");
+                                foreach ($kelas as $ks) {
+                                    echo "<option value='$ks[id_kelompok]'> $ks[nama_kelompok]</option>";
+                                    ?>
+                                <?php } ?>
+                            </select>
                         </div>
 
                         <div class="form-group">
                             <label>Kelompok</label>
-                            <input name="kelompok" type="text" class="form-control" placeholder="1">
+                            <select name="kelompok" id="" class="form-control">
+                                <option value="" disabled selected>-- Pilih Kelompok --</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                            </select>
+
                         </div>
 
                         <div class="form-group">
@@ -72,17 +118,48 @@
 
                         <div class="form-group">
                             <label>Waktu Mulai</label>
-                            <input name="tanggal" type="time" class="form-control" placeholder="5">
+                            <select name="wk_masuk" id="" class="form-control">
+                                <option value="" disabled selected>-- Pilih --</option>
+                                <option value="08.00">08.00</option>
+                                <option value="09.30">09.30</option>
+                                <option value="11.00">11.00</option>
+                                <option value="12.30">12.30</option>
+                                <option value="14.00">14.00</option>
+                                <option value="15.30">15.30</option>
+                                <option value="17.00">17.00</option>
+                                <option value="19.00">19.00</option>
+                                <option value="20.30">20.30</option>
+                            </select>
                         </div>
 
                         <div class="form-group">
                             <label>Waktu Selesai</label>
-                            <input name="tanggal" type="time" class="form-control" placeholder="5">
+                            <select name="wk_masuk" id="" class="form-control">
+                                <option value="" disabled selected>-- Pilih --</option>
+                                <option value="09.30">09.30</option>
+                                <option value="11.00">11.00</option>
+                                <option value="12.30">12.30</option>
+                                <option value="14.00">14.00</option>
+                                <option value="15.30">15.30</option>
+                                <option value="17.00">17.00</option>
+                                <option value="19.00">19.00</option>
+                                <option value="20.30">20.30</option>
+                                <option value="22.00">22.00</option>
+                            </select>
                         </div>
 
                         <div class="form-group">
                             <label>Ruangan</label>
-                            <input name="tanggal" type="text" class="form-control" placeholder="Lab Pemrograman">
+                            <select name="lab" class="form-control" required>
+                                <option value="">- Pilih -</option>
+                                <?php
+                                $matakuliah = mysqli_query($con, "SELECT * FROM tb_laboratorium ORDER BY id_lab ASC");
+                                foreach ($matakuliah as $g) {
+                                    echo "<option value='$g[id_lab]'>[ $g[kd_lab] ] $g[nama_lab]</option>";
+                                }
+                                ?>
+
+                            </select>
                         </div>
                         <div class="form-group">
                             <label>Jumlah Peserta Yang Hadir</label>
@@ -104,9 +181,48 @@
                             <input name="nama" type="text" class="form-control" placeholder="Nama dan Gelar">
                         </div>
 
+                        <div class="mt-3" id="select-container">
+                            <label for="kode_aslab">Pilih Kode Aslab:</label>
+                            <select class="form-control" id="kode_aslab1" name="kode_aslab[]" required>
+                                <option value="" disabled selected>Pilih kode aslab</option>
+                                <option value="ER">ER</option>
+                                <option value="DF">DF</option>
+                                <option value="RN">RN</option>
+                                <option value="RZ">RZ</option>
+                                <option value="ZD">ZD</option>
+                                <option value="TA">TA</option>
+                                <option value="FI">FI</option>
+                                <option value="AD">AD</option>
+                                <option value="NK">NK</option>
+                                <option value="DY">DY</option>
+                                <option value="SG">SG</option>
+                                <option value="BY">BY</option>
+                                <option value="FO">FO</option>
+                                <option value="FZ">FZ</option>
+                                <option value="SP">SP</option>
+                                <option value="RF">RF</option>
+                                <option value="MC">MC</option>
+                                <option value="SK">SK</option>
+                                <option value="HF">HF</option>
+                                <option value="RG">RG</option>
+                                <!-- Tambahkan pilihan kode aslab lainnya sesuai kebutuhan -->
+                            </select>
+                        </div>
+
+
+                        <!-- Laboratorium -->
                         <div class="form-group">
-                            <label>Nama Aslab</label>
-                            <input name="nama" type="text" class="form-control" placeholder="Nama dan Gelar">
+                            <label>Dosen</label>
+                            <select name="lab" class="form-control" required>
+                                <option value="">- Pilih -</option>
+                                <?php
+                                $matakuliah = mysqli_query($con, "SELECT * FROM tb_laboratorium ORDER BY id_lab ASC");
+                                foreach ($matakuliah as $g) {
+                                    echo "<option value='$g[id_lab]'>[ $g[kd_lab] ] $g[nama_lab]</option>";
+                                }
+                                ?>
+
+                            </select>
                         </div>
 
                         <div class="form-group">
@@ -123,6 +239,77 @@
         </div>
     </div>
 </div>
+
+
+<script>
+    // Mendapatkan elemen HTML yang diperlukan
+    const selectContainer = document.getElementById("select-container");
+
+    // Daftar pilihan kode aslab yang dapat ditampilkan
+    const kodeAslabOptions = [
+        "ER",
+        "DF",
+        "RN",
+        "RZ",
+        "ZD",
+        "TA",
+        "FI",
+        "AD",
+        "NK",
+        "DY",
+        "SG",
+        "BY",
+        "FO",
+        "FZ",
+        "SP",
+        "RF",
+        "MC",
+        "SK",
+        "HF",
+        "RG",
+    ];
+
+    // Event listener untuk menambahkan input select baru saat kode aslab pertama dipilih
+    document.addEventListener("change", function (event) {
+        // Cek apakah perubahan terjadi pada input select yang memiliki class "form-control"
+        if (
+            event.target.tagName === "SELECT" &&
+            event.target.classList.contains("form-control")
+        ) {
+            tambahInputSelect();
+        }
+    });
+
+    // Fungsi untuk menambahkan input select baru
+    function tambahInputSelect() {
+        const div = document.createElement("div");
+        div.classList.add("select-group"); // Tambahkan class "select-group" ke div baru
+        const select = document.createElement("select");
+        select.classList.add("form-control"); // Tambahkan class "form-control" ke input select
+        select.name = "kode_aslab[]";
+
+        // Tambahkan opsi pertama dengan nilai kosong (kosong yang terpilih)
+        const emptyOption = document.createElement("option");
+        emptyOption.value = "";
+        emptyOption.textContent = "Pilih kode aslab";
+        emptyOption.selected = true; // Buat yang pertama terpilih
+        emptyOption.disabled = true; // Jadikan yang pertama disabled
+        select.appendChild(emptyOption);
+
+        kodeAslabOptions.forEach((option) => {
+            const optionElement = document.createElement("option");
+            optionElement.value = option;
+            optionElement.textContent = option;
+            select.appendChild(optionElement);
+        });
+
+        div.appendChild(select);
+        selectContainer.appendChild(div);
+    }
+</script>
+
+
+
 
 <?php
 // Mengambil data dari formulir
